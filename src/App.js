290124a3d4bab99a730f1901c5c060a19a5db7a4
ShubhankarRaj9 <sparshrajsr@gmail.com>
2025-08-18 09:29:52 +0530

    update

--- src/App.js
+++ src/App.js
@@ -1,24 +1,24 @@
-import logo from './logo.svg';
+import React from 'react';
+import {Routes, Route, Link} from 'react-router-dom';
 import './App.css';
 
+import HomePage from './pages/homepage/homepage.component.jsx';
+
+const HatsPage = () =>{
+    return (
+      <div>
+        <h1>HATS PAGE</h1>
+      </div>
+    )
+};
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <div>
+      <Routes>
+          <Route  path='/' Component={HomePage} />
+          <Route  path='/hats' Component={HatsPage} />
+      </Routes>
+      </div>
   );
 }
 
